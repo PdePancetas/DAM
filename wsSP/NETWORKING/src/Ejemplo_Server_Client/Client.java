805d57f766686f3516a683e865fd0fbc5232bf13
PdePancetas <blancomiguel.bj@gmail.com>
2024-12-10 17:27:53 +0100

    networking ej 1-4

--- wsSP/NETWORKING/src/Ejemplo_Server_Client/Client.java
+++ wsSP/NETWORKING/src/Ejemplo_Server_Client/Client.java
@@ -1,6 +1,8 @@
 package Ejemplo_Server_Client;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -11,11 +13,15 @@ public class Client {
 		InetSocketAddress isa = new InetSocketAddress("localhost", 3333);
 		try {
 			s.connect(isa);
-			OutputStream os = s.getOutputStream();
-			OutputStreamWriter osw = new OutputStreamWriter(os);
-			osw.write("Hooola");
+			BufferedWriter osw = new BufferedWriter( new OutputStreamWriter(s.getOutputStream()));
+			BufferedReader isr = new BufferedReader( new InputStreamReader(s.getInputStream()));
+			osw.write("C: Hooola. desde el puerto "+isa.getPort()+"\n");
 			osw.flush();
-			os.close();
+			String data = isr.readLine();
+			System.out.println("C: recibido: " + new String(data));
+			
+			isr.close();
+			osw.close();
 			s.close();
 		} catch (IOException e) {
 			e.printStackTrace();
--- wsSP/NETWORKING/src/Ejemplo_Server_Client/Server.java
+++ wsSP/NETWORKING/src/Ejemplo_Server_Client/Server.java
@@ -1,28 +1,36 @@
 package Ejemplo_Server_Client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.*;
 
 public class Server {
 	public static void main(String[] args) {
 		try {
-			System.out.println("Creando Socket Servidor");
+			System.out.println("S: Creando Socket Servidor");
 			ServerSocket ss = new ServerSocket();
-			System.out.println("Bind");
+			System.out.println("S: Bind");
 			InetSocketAddress addr = new InetSocketAddress("localhost", 3333);
 			ss.bind(addr);
 			while (true) {
-				System.out.println("accept");
+				System.out.println("S: accept");
 				Socket nsc = ss.accept();
-				System.out.println("conexión recibida");
-				InputStream is = nsc.getInputStream();
-				OutputStream os = nsc.getOutputStream();
-				byte[] mensaje = new byte[25];
-				is.read(mensaje);
-				System.out.println("recibido:" + new String(mensaje));
-				System.out.println("Esperando nueva conexion...");
+				System.out.println("S: conexión recibida");
+				BufferedReader br = new BufferedReader(new InputStreamReader(nsc.getInputStream()));
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(nsc.getOutputStream()));
+				String data = br.readLine();
+				System.out.println("S: recibido: " + new String(data));
+				bw.write("S: Adioss \n");
+				bw.flush();
+				System.out.println("S: Esperando nueva conexion...");
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
