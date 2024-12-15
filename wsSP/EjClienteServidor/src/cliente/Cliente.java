ed2383a47fa753810cabcf102c22bcc69ff00ec9
PdePancetas <blancomiguel.bj@gmail.com>
2024-12-15 13:28:09 +0100

    EjClienteServidor v1

--- wsSP/EjClienteServidor/src/cliente/Cliente.java
+++ wsSP/EjClienteServidor/src/cliente/Cliente.java
@@ -2,9 +2,10 @@ package cliente;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -15,72 +16,87 @@ import utilidadesTeclado.Teclado;
 public class Cliente {
 
 	public static void main(String[] args) {
-
 		Socket s = new Socket();
 		InetSocketAddress isa = new InetSocketAddress("localhost", 3333);
 
 		try {
 			s.connect(isa);
+			System.out.println("S: conexión recibida");
 			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-			ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+			DataInputStream dis = new DataInputStream(s.getInputStream());
 
 			// Opciones dadas por el servidor
-
+			ArrayList<String> opciones = new ArrayList<>();
+			System.out.println("Opciones:");
 			String linea = "";
+
 			while ((linea = br.readLine()) != null) {
 				System.out.println(linea);
+
+				opciones.add(linea.split(" ")[0]);
+				if (linea.split(" ")[0].equals("salir")) { // Detectar fin de bloque
+					break; // Sale del bucle
+				}
 			}
-//			do {
-//				op = Teclado.leerEntero();
-//
-//				// El servidor obtendra una linea de la cual leerá la opcion
-//				// y ejecutara las tareas correspondientes
-//				switch (op) {
-//				case 1:// Opcion 1: Manda una orden al servidor para crear un fichero
-//					LocalDateTime ldt = LocalDateTime.now();
-//					String nombre = Teclado.leerCadena();
-//					bw.write(op + "|" + nombre + "-" + ldt.format(DateTimeFormatter.ofPattern("hh-mm-ss")));
-//					bw.newLine();
-//					bw.flush();
-//					System.out.println("S: " + br.readLine());
-//					break;
-//				case 2:// Opcion 1: Manda una orden al servidor para bajar un fichero
-//					ldt = LocalDateTime.now();
-//					bw.write(op + "|" + "fichero-" + ldt.format(DateTimeFormatter.ofPattern("hh-mm-ss")));
-//					bw.newLine();
-//					bw.flush();
-//					System.out.println("S: " + br.readLine());
-//					try {
-//						File f = (File) ois.readObject();
-//						System.out.println("Recibido el fichero: " + f.getName());
-//					} catch (ClassNotFoundException e) {
-//						e.printStackTrace();
-//					}
-//
-//					break;
-//				case 0: // Opcion 0: Desconectarse del servidor
-//					System.out.println("Desconectando");
-//					s.close();
-//					break;
-//				default: // Si la opcion no es valida, vuelve a mostrar el menu
-//					System.out.println("Opcion incorrecta\n");
-//					break;
-//				}
-//
-//			} while (op != 0);
-			bw.write(Teclado.leerCadena());
+			// Se le pide al cliente que introduzca una opcion hasta que elija salir
+			String opcion = "";
+			do {
+				System.out.print("-> ");
+				opcion = Teclado.leerCadena();
 
-			String respuesta = br.readLine();
+				if (opciones.contains(opcion.split(" ")[0])) {
 
-			System.out.println("S: " + respuesta);
+					bw.write(opcion);
+					bw.newLine();
+					bw.flush();
+					String respuesta = "";
+					if (opcion.split(" ")[0].equalsIgnoreCase("bajar"))
+						recibirCopiaArchivo(opcion.split(" ")[1], dis, br);
+					else
+						respuesta = br.readLine();
+
+					System.out.println("S: " + respuesta);
+				} else
+					System.out.println("Opcion no valida");
+			} while (!opcion.split(" ")[0].equals("salir"));
 
 			br.close();
 			bw.close();
-			ois.close();
+			dis.close();
+			s.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 
+	private static void recibirCopiaArchivo(String nombre, DataInputStream dis, BufferedReader br) {
+		try {
+			String mensaje = br.readLine();
+			System.out.println("Servidor: " + mensaje);
+			if (mensaje.equalsIgnoreCase("Enviando archivo...")) {
+				// Leer tamaño del archivo
+				long tamañoArchivo = dis.readLong();
+				System.out.println("Tamaño del archivo: " + tamañoArchivo + " bytes");
+
+				// Crear copia local del archivo
+				try (FileOutputStream fileOutputStream = new FileOutputStream("downloads/" + nombre)) {
+					byte[] buffer = new byte[4096];
+					int bytesRead;
+					long totalBytesRead = 0;
+
+					while (totalBytesRead < tamañoArchivo && (bytesRead = dis.read(buffer)) != -1) {
+						fileOutputStream.write(buffer, 0, bytesRead);
+						totalBytesRead += bytesRead;
+					}
+					System.out.println("Archivo recibido y guardado: downloads/" + nombre);
+				}
+			} else {
+				System.out.println("Servidor: " + mensaje);
+			}
+		} catch (IOException e) {
+
+		}
+	}
 }
--- wsSP/EjClienteServidor/src/servidor/Server.java
+++ wsSP/EjClienteServidor/src/servidor/Server.java
@@ -1,6 +1,132 @@
 package servidor;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
 public class Server {
 
-	
+	public static void main(String[] args) {
+		try {
+			System.out.println("S: Creando Socket Servidor");
+			try (ServerSocket ss = new ServerSocket()) {
+				System.out.println("S: Bind");
+				InetSocketAddress addr = new InetSocketAddress("localhost", 3333);
+				ss.bind(addr);
+				while (true) {
+					System.out.println("S: accept");
+					Socket nsc = ss.accept();
+					boolean clienteActivo = true;
+					BufferedReader br = new BufferedReader(new InputStreamReader(nsc.getInputStream()));
+					BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(nsc.getOutputStream()));
+					ObjectOutputStream ois = new ObjectOutputStream(nsc.getOutputStream());
+					DataOutputStream dos = new DataOutputStream(nsc.getOutputStream());
+					
+					bw.write("crear (nombre_archivo)");
+					bw.newLine();
+					bw.write("bajar (nombre_archivo)");
+					bw.newLine();
+					bw.write("salir ");
+					bw.newLine();
+					bw.flush();
+
+					while (clienteActivo)
+						try {
+							String opcionRespuesta = br.readLine();
+							switch (opcionRespuesta.split(" ")[0]) {
+							case "crear":
+
+								crearArchivo(opcionRespuesta.split(" ")[1], bw);
+								break;
+							case "bajar":
+								enviarCopia(opcionRespuesta.split(" ")[1], dos, bw);
+								break;
+							case "salir":
+								bw.write("Conexion finalizada");
+								bw.newLine();
+								bw.flush();
+								clienteActivo = false;
+								break;
+							}
+						} catch (Exception e) {
+
+						}
+					br.close();
+					bw.close();
+					ois.close();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void crearArchivo(String nombre, BufferedWriter bw) {
+
+		LocalDateTime fecha = LocalDateTime.now();
+
+		File fichero = new File("datos/" + nombre);
+		try {
+			fichero.createNewFile();
+			FileWriter fr = new FileWriter(fichero);
+			fr.write("Creado en: " + fecha);
+			fr.close();
+			bw.write("Fichero creado con exito");
+			bw.newLine();
+			bw.flush();
+		} catch (IOException e) {
+			try {
+				bw.write("No se pudo crear el archivo: " + e.getMessage());
+				bw.newLine();
+				bw.flush();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+		}
+	}
+
+	private static void enviarCopia(String nombre, DataOutputStream dos, BufferedWriter bw)
+			throws IOException {
+		File archivo = new File("datos/"+nombre);
+		if (archivo.exists()) {
+			
+			bw.write("Enviando archivo...");
+			bw.newLine();
+			bw.flush();
+
+			
+			dos.writeLong(archivo.length());
+			dos.flush();
+
+			
+			try (FileInputStream fis = new FileInputStream(archivo)) {
+				byte[] buffer = new byte[(int) archivo.length()];
+				int bytesRead;
+				while ((bytesRead = fis.read(buffer)) != -1) {
+					dos.write(buffer, 0, bytesRead);
+				}
+				dos.flush();
+			}
+			System.out.println("Archivo enviado: " + nombre);
+		} else {
+			bw.write("Error: El archivo '" + nombre + "' no existe.");
+			bw.newLine();
+			bw.flush();
+		}
+	}
+
 }
