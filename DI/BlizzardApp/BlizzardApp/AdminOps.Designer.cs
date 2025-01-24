cf0cdf6982710c8bde59361f6ccd7ef11bc206b8
PdePancetas <blancomiguel.bj@gmail.com>
2025-01-24 14:25:27 +0100

    AdminOps

--- DI/BlizzardApp/BlizzardApp/AdminOps.Designer.cs
+++ DI/BlizzardApp/BlizzardApp/AdminOps.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnMostrarUsuarios = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGuardarCambios = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,11 +52,22 @@
             this.dataGridView1.Size = new System.Drawing.Size(434, 252);
             this.dataGridView1.TabIndex = 2;
             // 
+            // btnGuardarCambios
+            // 
+            this.btnGuardarCambios.Location = new System.Drawing.Point(604, 296);
+            this.btnGuardarCambios.Name = "btnGuardarCambios";
+            this.btnGuardarCambios.Size = new System.Drawing.Size(130, 23);
+            this.btnGuardarCambios.TabIndex = 3;
+            this.btnGuardarCambios.Text = "Aplicar cambios";
+            this.btnGuardarCambios.UseVisualStyleBackColor = true;
+            this.btnGuardarCambios.Click += new System.EventHandler(this.btnGuardarDatos_Click);
+            // 
             // AdminOps
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardarCambios);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnMostrarUsuarios);
             this.Name = "AdminOps";
@@ -68,5 +80,6 @@
         #endregion
         private System.Windows.Forms.Button btnMostrarUsuarios;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGuardarCambios;
     }
 }
--- DI/BlizzardApp/BlizzardApp/AdminOps.cs
+++ DI/BlizzardApp/BlizzardApp/AdminOps.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,7 +24,7 @@ namespace BlizzardApp
         private void btnMostrarUsuarios_Click(object sender, EventArgs e)
         {
             string connectionString = "Server=localhost;Database=prueba_users;User ID=root;Password=1234";
-            string query = "SELECT name, email, password FROM users";
+            string query = "SELECT name, email, password, rol FROM users";
 
             using(MySqlConnection conection = new MySqlConnection(connectionString))
             {
@@ -33,6 +35,58 @@ namespace BlizzardApp
 
 
                 dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void btnGuardarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "Server=localhost;Database=prueba_users;User ID=root;Password=1234";
+                int totalFilasAgregadas = 0;
+                for (int i=0;i<dataGridView1.Rows.Count-1;i++)
+                {
+                    string nombre = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    string email = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    string password = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                    string rol = dataGridView1.Rows[i].Cells[3].Value.ToString();
+
+                    if (!rol.Equals("user") || !rol.Equals("admin"))
+                        throw new Exception();
+                    else if (rol.Equals("admin") && !password.Equals("Admin1234"))
+                        throw new Exception();
+                    else
+                    {
+                        using (MySqlConnection conection = new MySqlConnection(connectionString))
+                        {
+                            conection.Open();
+                            MySqlCommand usuarioEncontrado = new MySqlCommand("Select name, email, password from users where name=@name AND email=@email AND password=@password", conection);
+                            usuarioEncontrado.Parameters.AddWithValue("name", nombre);
+                            usuarioEncontrado.Parameters.AddWithValue("email", email);
+                            usuarioEncontrado.Parameters.AddWithValue("password", password);
+
+                            object filasRegresadas = usuarioEncontrado.ExecuteScalar();
+
+                            if (filasRegresadas == null)
+                            {
+                                MySqlCommand agregarUsuario = new MySqlCommand("Insert into users (name,email,password,rol) values (@name, @email, @password, @rol)", conection);
+                                agregarUsuario.Parameters.AddWithValue("name", nombre);
+                                agregarUsuario.Parameters.AddWithValue("email", email);
+                                agregarUsuario.Parameters.AddWithValue("password", password);
+                                agregarUsuario.Parameters.AddWithValue("rol", rol);
+                                int filasAgregadas = agregarUsuario.ExecuteNonQuery();
+                                totalFilasAgregadas++;
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show($"Filas añadidas: {totalFilasAgregadas}", "Mensaje!!", MessageBoxButtons.OK);
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Ha ocurrido un error, comprueba que los datos están bien introducidos!",null,MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
