915e7fc35ba248cf38ba03f2c88446aad6c8d630
PdePancetas <blancomiguel.bj@gmail.com>
2025-01-24 13:19:41 +0100

    Mostrar Usuarios

--- DI/BlizzardApp/BlizzardApp/AdminOps.Designer.cs
+++ DI/BlizzardApp/BlizzardApp/AdminOps.Designer.cs
@@ -28,50 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMostrarUsuarios = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // btnMostrarUsuarios
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 177F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(275, 40);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(501, 265);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.btnMostrarUsuarios.Location = new System.Drawing.Point(12, 12);
+            this.btnMostrarUsuarios.Name = "btnMostrarUsuarios";
+            this.btnMostrarUsuarios.Size = new System.Drawing.Size(109, 31);
+            this.btnMostrarUsuarios.TabIndex = 1;
+            this.btnMostrarUsuarios.Text = "Mostrar Usuarios";
+            this.btnMostrarUsuarios.UseVisualStyleBackColor = true;
+            this.btnMostrarUsuarios.Click += new System.EventHandler(this.btnMostrarUsuarios_Click);
             // 
-            // button1
+            // dataGridView1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 31);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(300, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(434, 252);
+            this.dataGridView1.TabIndex = 2;
             // 
             // AdminOps
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnMostrarUsuarios);
             this.Name = "AdminOps";
             this.Text = "AdminOps";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMostrarUsuarios;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- DI/BlizzardApp/BlizzardApp/AdminOps.cs
+++ DI/BlizzardApp/BlizzardApp/AdminOps.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySqlConnector;
 
 namespace BlizzardApp
 {
@@ -18,9 +19,21 @@ namespace BlizzardApp
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnMostrarUsuarios_Click(object sender, EventArgs e)
         {
+            string connectionString = "Server=localhost;Database=prueba_users;User ID=root;Password=1234";
+            string query = "SELECT name, email, password FROM users";
 
+            using(MySqlConnection conection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, conection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+
+                dataGridView1.DataSource = dt;
+            }
         }
     }
 }
